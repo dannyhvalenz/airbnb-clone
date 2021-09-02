9b24e062fddc7094e5af30bb3b94cd70037e507e
Daniela H Valenzuela <dannyhvalenz@gmail.com>
2021-09-01 19:50:31 -0500

    changes on community section

--- components/Footer.js
+++ components/Footer.js
@@ -9,28 +9,17 @@ function Footer() {
                 <p>Inversores</p>
                 <p>Airbnb Plus</p>
                 <p>Airbnb Luxe</p>
-                <p>Airbnb para viajes de trabajo</p>
-                <p>Los anfitriones hacen lo posible</p>
-                <p>Empleo</p>
-                <p>Carta de los fundadores</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
             <h5 className="uppercase font-bold">Comunidad</h5>
-                <p>Diversidad</p>
-                <p>Accesibilidad</p>
-                <p>Colaboradores de Airbnb</p>
-                <p>Acoge a refugiados afganos</p>
-                <p>Invitaciones para huéspedes</p>
-                <p>Airbnb.org</p>
+                <p className="w-52">Esto es un proyecto clon de Airbnb</p>
+                <p className="w-52">Busca mostrar mis capacidades como desarrollador</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
             <h5 className="uppercase font-bold">Anfitrión</h5>
             <p>Hospeda en tu alojamiento</p>
             <p>Ofrece una Experiencia en línea</p>
             <p>Ofrece una Experiencia</p>
-            <p>Ser un anfitrión responsable</p>
-            <p>Centro de recursos</p>
-            <p>Centro de la comunidad</p>
             </div>
             <div className="space-y-4 text-xs text-gray-800">
             <h5 className="uppercase font-bold">Asistencia</h5>
