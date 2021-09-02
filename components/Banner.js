0ac8f92be37fe4c50b62c171c0411d56de296cfa
Daniela H Valenzuela <dannyhvalenz@gmail.com>
2021-09-01 19:55:36 -0500

    changes to BANNER

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
@@ -8,12 +8,15 @@ function Banner() {
                 objectFit="cover"
             />
             <div className="absolute top-1/2 w-full text-center">
-                <p className="text-sm sm:text-lg">¿No sabes a donde ir?</p>
-                <button className="bg-white text-purple-500 font-bold my-3 rounded-full shadow-md px-10 py-4 hover:shadow-xl active:scale-90 transition duration-150">Soy Flexible</button>
+                <p className="text-sm sm:text-lg">
+                    ¿No sabes a donde ir? No importa
+                </p>
+                <button className="bg-white text-purple-500 font-bold my-3 rounded-full shadow-md px-10 py-4 hover:shadow-xl active:scale-90 transition duration-150">
+                    Soy Flexible
+                </button>
             </div>
-            
         </div>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
